c800244c4503803a49367b07b3e8cd8f496d0e90
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-01-13 11:06:54 +0200

    Fix eslint error

--- src/Ship.js
+++ src/Ship.js
@@ -1,17 +1,17 @@
-export default function Ship({length}) {
-    let hitCount = 0;
+export default function Ship({ length }) {
+  let hitCount = 0;
 
-    function hit() {
-        hitCount++;
-    }
+  function hit() {
+    hitCount += 1;
+  }
 
-    function isSunk() {
-        return hitCount === length;
-    }
+  function isSunk() {
+    return hitCount === length;
+  }
 
-    return {
-        length, 
-        hit, 
-        isSunk
-    }
+  return {
+    length,
+    hit,
+    isSunk,
+  };
 }
