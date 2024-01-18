88113fd707c8470b6d64ecb70e9a6b21b406ffb6
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-01-18 10:56:57 +0200

    Update computer get move logic to try adjacent slots after getting a ‘hit’.

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,10 +1,53 @@
 export default function Player({ name, gameboard, isComputer = false }) {
+  let hitRow;
+  let hitCol;
+
+  function adjacentMoves() {
+    const moves = [];
+    if (hitRow + 1 < 10) {
+      moves.push([hitRow + 1, hitCol]);
+    }
+    if (hitRow - 1 >= 0) {
+      moves.push([hitRow - 1, hitCol]);
+    }
+    if (hitCol + 1 < 10) {
+      moves.push([hitRow, hitCol + 1]);
+    }
+    if (hitCol - 1 >= 0) {
+      moves.push([hitRow, hitCol - 1]);
+    }
+    return moves;
+  }
+
   function getMove() {
+    if (hitRow && hitCol) {
+      const moves = adjacentMoves();
+      const allMovesMade = moves.every(
+        (move) =>
+          gameboard.getBoard()[move[0]][move[1]] === 'hit' ||
+          gameboard.getBoard()[move[0]][move[1]] === 'miss'
+      );
+
+      if (allMovesMade) {
+        hitRow = null;
+        hitCol = null;
+        return getMove();
+      }
+
+      let randInx = Math.floor(Math.random() * moves.length);
+      while (
+        gameboard.getBoard()[moves[randInx][0]][moves[randInx][1]] === 'hit' ||
+        gameboard.getBoard()[moves[randInx][0]][moves[randInx][1]] === 'miss'
+      ) {
+        randInx = Math.floor(Math.random() * moves.length);
+      }
+      return moves[randInx];
+    }
     let randRow = Math.floor(Math.random() * 10);
     let randCol = Math.floor(Math.random() * 10);
     while (
-      gameboard.getBoard()[randRow][randCol] === 'hit' ||
-      gameboard.getBoard()[randRow][randCol] === 'miss'
+      gameboard.getBoard()[randRow][randCol] === 'miss' ||
+      gameboard.getBoard()[randRow][randCol] === 'hit'
     ) {
       randRow = Math.floor(Math.random() * 10);
       randCol = Math.floor(Math.random() * 10);
@@ -12,6 +55,7 @@ export default function Player({ name, gameboard, isComputer = false }) {
 
     return [randRow, randCol];
   }
+
   function attack(row, col) {
     if (!isComputer) {
       gameboard.receiveAttack(row, col);
@@ -20,6 +64,11 @@ export default function Player({ name, gameboard, isComputer = false }) {
 
     const [computerRow, computerCol] = getMove();
     gameboard.receiveAttack(computerRow, computerCol);
+    if (gameboard.getBoard()[computerRow][computerCol] === 'hit') {
+      hitRow = computerRow;
+      hitCol = computerCol;
+    }
+
     return [computerRow, computerCol];
   }
   return {
--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -14,3 +14,51 @@ test('Gameboard receiveAttack is called when attack', () => {
   player.attack(0, 0);
   expect(receiveAttack).toHaveBeenCalledWith(0, 0);
 });
+
+test('Computer tries adjacent slots after a hit', () => {
+  const board = Array.from({ length: 10 }).fill(Array(10));
+  const player1Board = {
+    getBoard: () => board,
+    receiveAttack: jest.fn((row, col) => {
+      board[row][col] = 'hit';
+    }),
+  };
+  const player = Player({
+    name: 'Computer',
+    gameboard: player1Board,
+    isComputer: true,
+  });
+
+  const [row1, col1] = player.attack();
+  const [row2, col2] = player.attack();
+  expect(
+    (row1 === row2 && Math.abs(col1 - col2) === 1) ||
+      (col1 === col2 && Math.abs(row1 - row2) === 1)
+  ).toBeTruthy();
+});
+
+test('Computer tries adjacent slots after a hit but not same move', () => {
+  const board = Array.from({ length: 10 }).fill(Array(10));
+  const player1Board = {
+    getBoard: () => board,
+    receiveAttack: jest.fn((row, col) => {
+      board[row][col] = 'hit';
+    }),
+  };
+  const player = Player({
+    name: 'Computer',
+    gameboard: player1Board,
+    isComputer: true,
+  });
+
+  const move1 = player.attack();
+  const move2 = player.attack();
+  const move3 = player.attack();
+  const move4 = player.attack();
+  const move5 = player.attack();
+
+  expect(move1).not.toEqual(move2);
+  expect(move2).not.toEqual(move3);
+  expect(move3).not.toEqual(move4);
+  expect(move4).not.toEqual(move5);
+});
